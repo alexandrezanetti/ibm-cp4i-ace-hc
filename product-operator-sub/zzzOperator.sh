128453754d392dfc1d1e2606bbb4aa41e9b0d67a
alexandrezanetti <alexandre_zanetti_almeida@yahoo.com.br>
2023-03-02 11:07:58 -0300

    Update zzzOperator.sh

--- product-operator-sub/zzzOperator.sh
+++ product-operator-sub/zzzOperator.sh
@@ -7,19 +7,8 @@ echo $CHANNEL
 mkdir -p /root/cp4i/$OPERATOR
 chmod 777 /root/cp4i/$OPERATOR
 
-cat /root/cp4i/$OPERATOR/$OPERATOR-operator.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/$PROJECT/g"  | sed "s/{###PROVIDE_YOUR_OPERATOR_PRODUCT_CP4X_HERE###}/$OPERATOR/g" >/root/cp4i/$OPERATOR/$OPERATOR_OK.yaml<br>
+cat /root/cp4i/$OPERATOR/$OPERATOR-operator.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/$PROJECT/g"  | sed "s/{###PROVIDE_YOUR_OPERATOR_PRODUCT_CP4X_HERE###}/$OPERATOR/g" >/root/cp4i/$OPERATOR/$OPERATOR_OK.yaml
 cat /root/cp4i/$OPERATOR/$OPERATOR-sub.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/$PROJECT/g" | sed "s/{###PROVIDE_CHANNEL_OPERATOR_HERE###}/$CHANNEL/g" | sed "s/{###PROVIDE_YOUR_OPERATOR_PRODUCT_CP4X_HERE###}/$OPERATOR/g" >/root/cp4i/$OPERATOR/$OPERATOR-sub_OK.yaml
 
-#### 8. Execute o script / And finally, run the script
-> oc apply -f /root/cp4i/$OPERATOR/$OPERATOR-operator_OK.yaml<br>
-> oc apply -f /root/cp4i/$OPERATOR/$OPERATOR-sub_OK.yaml
-
-#### Informação complementar / Addicional information
-- Como descobrir os operadores / How to discover the operators:
-> oc get packagemanifests --all-namespaces | grep "IBM Operator Catalog " | grep "ibm-integration-platform-navigator"<br>
-> oc get packagemanifests --all-namespaces | grep "IBM Operator Catalog " | grep "ibm-common-service-operator"<br>
-> oc get packagemanifests --all-namespaces | grep "IBM Operator Catalog " | grep "ibm-mq"<br>
-> oc get packagemanifests --all-namespaces | grep "IBM Operator Catalog " | grep "aspera-hsts-operator"<br>
-> oc get packagemanifests --all-namespaces | grep "IBM Operator Catalog " | grep "ibm-apiconnect"<br>
-> oc get packagemanifests --all-namespaces | grep "IBM Operator Catalog " | grep "ibm-appconnect"<br>
-> oc get packagemanifests --all-namespaces | grep "IBM Operator Catalog " | grep "ibm-events-operator"
+oc apply -f /root/cp4i/$OPERATOR/$OPERATOR-operator_OK.yaml
+oc apply -f /root/cp4i/$OPERATOR/$OPERATOR-sub_OK.yaml
