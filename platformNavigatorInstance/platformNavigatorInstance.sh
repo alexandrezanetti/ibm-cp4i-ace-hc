dc223e7a8a549f0ff821b25f744d11de819a99b4
alexandrezanetti <alexandre_zanetti_almeida@yahoo.com.br>
2023-03-02 17:01:13 -0300

    Update platformNavigatorInstance.sh

--- platformNavigatorInstance/platformNavigatorInstance.sh
+++ platformNavigatorInstance/platformNavigatorInstance.sh
@@ -6,6 +6,6 @@ echo $STORAGECLASSFS
 mkdir -p /tmp/$PROJECT/platformNavigatorInstance
 chmod 777 /tmp/$PROJECT/platformNavigatorInstance
 
-cat /root/platformNavigatorInstance/platformNavigatorInstance.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" | sed "s/{###PROVIDE_YOUR_STORAGECLASSFS_HERE###}/$STORAGECLASSFS/g"  | sed "s/{###PROVIDE_YOUR_OPERATOR_PRODUCT_CP4X_HERE###}/$OPERATOR/g" >/tmp/$PROJECT/platformNavigatorInstance/platformNavigatorInstance_OK.yaml
+cat /root/platformNavigatorInstance/platformNavigatorInstance.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" | sed "s/{###PROVIDE_YOUR_STORAGECLASSFS_HERE###}/$STORAGECLASSFS/g"  >/tmp/$PROJECT/platformNavigatorInstance/platformNavigatorInstance_OK.yaml
  
 oc apply -f /tmp/$PROJECT/platformNavigatorInstance/platformNavigatorInstance_OK.yaml
