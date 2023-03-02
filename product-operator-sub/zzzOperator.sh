3d1529fcdbd72bd2eb7c5db5335765bc52effe8f
alexandrezanetti <alexandre_zanetti_almeida@yahoo.com.br>
2023-03-02 11:33:15 -0300

    Update zzzOperator.sh

--- product-operator-sub/zzzOperator.sh
+++ product-operator-sub/zzzOperator.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 #channel versions: https://www.ibm.com/docs/en/cloud-paks/cp-integration/2022.4?topic=reference-operator-channel-versions-this-release
-echo $OPERATOR
+echo $PROJECT
 echo $CHANNEL
+echo $OPERATOR
+echo $PROJECT
 
-mkdir -p /tmp/cp4i/$OPERATOR
-chmod 777 /tmp/cp4i/$OPERATOR
+mkdir -p /tmp/$PROJECT/$OPERATOR
+chmod 777 /tmp/$PROJECT/$OPERATOR
 
-cat /root/cp4i/$OPERATOR/$OPERATOR-operator.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/$OPERATOR/g"  | sed "s/{###PROVIDE_YOUR_OPERATOR_PRODUCT_CP4X_HERE###}/$OPERATOR/g" >/tmp/cp4i/$OPERATOR-operator_OK.yaml
-cat /root/cp4i/$OPERATOR/$OPERATOR-sub.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${OPERATOR}/g" | sed "s/{###PROVIDE_CHANNEL_OPERATOR_HERE###}/$CHANNEL/g" | sed "s/{###PROVIDE_YOUR_OPERATOR_PRODUCT_CP4X_HERE###}/$OPERATOR/g" >/tmp/cp4i/$OPERATOR-sub_OK.yaml
+cat /root/$PROJECT/$OPERATOR/$OPERATOR-operator.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/$OPERATOR/g"  | sed "s/{###PROVIDE_YOUR_OPERATOR_PRODUCT_CP4X_HERE###}/$OPERATOR/g" >/tmp/$PROJECT/$OPERATOR-operator_OK.yaml
+cat /root/$PROJECT/$OPERATOR/$OPERATOR-sub.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${OPERATOR}/g" | sed "s/{###PROVIDE_CHANNEL_OPERATOR_HERE###}/$CHANNEL/g" | sed "s/{###PROVIDE_YOUR_OPERATOR_PRODUCT_CP4X_HERE###}/$OPERATOR/g" >/tmp/$PROJECT/$OPERATOR-sub_OK.yaml
 
-oc apply -f /tmp/cp4i/$OPERATOR-operator_OK.yaml
-oc apply -f /tmp/cp4i/$OPERATOR-sub_OK.yaml
+oc apply -f /tmp/$PROJECT/$OPERATOR-operator_OK.yaml
+oc apply -f /tmp/$PROJECT/$OPERATOR-sub_OK.yaml
