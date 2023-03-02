f780a6103138dd2dc6bc23b73cbe5be7f15b7235
alexandrezanetti <alexandre_zanetti_almeida@yahoo.com.br>
2023-03-02 11:26:09 -0300

    Update zzzOperator.sh

--- product-operator-sub/zzzOperator.sh
+++ product-operator-sub/zzzOperator.sh
@@ -7,7 +7,7 @@ echo $CHANNEL
 mkdir -p /tmp/cp4i/$OPERATOR
 chmod 777 /tmp/cp4i/$OPERATOR
 
-cat /root/cp4i/$OPERATOR/$OPERATOR-operator.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/$OPERATOR/g"  | sed "s/{###PROVIDE_YOUR_OPERATOR_PRODUCT_CP4X_HERE###}/$OPERATOR/g" >/tmp/cp4i/$OPERATOR_OK.yaml
+cat /root/cp4i/$OPERATOR/$OPERATOR-operator.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/$OPERATOR/g"  | sed "s/{###PROVIDE_YOUR_OPERATOR_PRODUCT_CP4X_HERE###}/$OPERATOR/g" >/tmp/cp4i/$OPERATOR-operator_OK.yaml
 cat /root/cp4i/$OPERATOR/$OPERATOR-sub.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${OPERATOR}/g" | sed "s/{###PROVIDE_CHANNEL_OPERATOR_HERE###}/$CHANNEL/g" | sed "s/{###PROVIDE_YOUR_OPERATOR_PRODUCT_CP4X_HERE###}/$OPERATOR/g" >/tmp/cp4i/$OPERATOR-sub_OK.yaml
 
 oc apply -f /tmp/cp4i/$OPERATOR-operator_OK.yaml
