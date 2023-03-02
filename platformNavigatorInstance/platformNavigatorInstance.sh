3131d6e5402f95c60f1b5a3521f50f33c23a9de4
alexandrezanetti <alexandre_zanetti_almeida@yahoo.com.br>
2023-03-02 16:46:51 -0300

    Create platformNavigatorInstance.sh

---
+++ platformNavigatorInstance/platformNavigatorInstance.sh
@@ -0,0 +1,21 @@
+PROJECT=cp4i
+STORAGECLASSFS=rook-cephfs
+cat <<EOF >> platformNavigatorInstance.yaml
+apiVersion: integration.ibm.com/v1beta1
+kind: PlatformNavigator
+metadata:
+  name: $PROJECT-platform-navigator-ui
+  labels:
+    app: $PROJECT-platform-navigator-ui
+  namespace: openshift-operators
+spec:
+  license:
+    accept: false
+    license: L-RJON-CJR2RX
+  storage:
+    class: $STORAGECLASSFS
+  mqDashboard: true
+  replicas: 1
+  version: 2022.4.1
+EOF
+oc apply -f platformNavigatorInstance.yaml
