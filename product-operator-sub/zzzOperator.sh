ce4b429aa452c5ab94ad595c590e11c793584781
alexandrezanetti <alexandre_zanetti_almeida@yahoo.com.br>
2023-03-02 11:17:03 -0300

    Update zzzOperator.sh

--- product-operator-sub/zzzOperator.sh
+++ product-operator-sub/zzzOperator.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #channel versions: https://www.ibm.com/docs/en/cloud-paks/cp-integration/2022.4?topic=reference-operator-channel-versions-this-release
 echo ${OPERATOR}
 echo ${CHANNEL}
