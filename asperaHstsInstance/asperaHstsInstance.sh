07ef219721be43feb47dd1e2836038dfe3c8877e
alexandrezanetti <alexandre_zanetti_almeida@yahoo.com.br>
2023-03-16 13:14:54 -0300

    Update asperaHstsInstance.sh

--- asperaHstsInstance/asperaHstsInstance.sh
+++ asperaHstsInstance/asperaHstsInstance.sh
@@ -13,7 +13,7 @@ oc apply -f /tmp/$PROJECT/asperaHstsInstance/asperaHstsInstancePvc_OK.yaml
 
 oc create secret generic asperanoded-creds --from-literal=NODE_USER=nodeuser --from-literal=NODE_PASS=`uuidgen` -n $PROJECT
 
-chmod a+x /root/cp4i/asperaHstsInstance/asperaHstsInstance.yaml
+chmod 777 /root/cp4i/asperaHstsInstance/asperaHstsInstance.yaml
 
 cat /root/cp4i/asperaHstsInstance/asperaHstsInstance.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" | sed "s/{###PROVIDE_YOUR_STORAGECLASSFS_HERE###}/$STORAGECLASSFS/g"  | sed "s/{###PROVIDE_YOUR_ASPERA_LICENCE_CP4X_HERE###}/$ASPERALICENCA/g"  | sed "s/{###PROVIDE_YOUR_ASPERA_DIRECTORY_HERE###}/$ASPERADIRECTORY/g"  >/tmp/$PROJECT/asperaHstsInstance/asperaHstsInstance_OK.yaml
 
