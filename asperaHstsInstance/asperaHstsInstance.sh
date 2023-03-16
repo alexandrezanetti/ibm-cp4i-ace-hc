d166dd137f11e04c5c346cd7337ddb44c51817b3
alexandrezanetti <alexandre_zanetti_almeida@yahoo.com.br>
2023-03-16 13:25:45 -0300

    Update asperaHstsInstance.sh

--- asperaHstsInstance/asperaHstsInstance.sh
+++ asperaHstsInstance/asperaHstsInstance.sh
@@ -3,7 +3,6 @@
 echo $PROJECT
 echo $STORAGECLASSFS
 echo $ASPERALICENCE
-echo $ASPERADIRECTORY
 
 mkdir -p /tmp/$PROJECT/asperaHstsInstance
 chmod a+x /tmp/$PROJECT/asperaHstsInstance
@@ -13,6 +12,5 @@ oc apply -f /tmp/$PROJECT/asperaHstsInstance/asperaHstsInstancePvc_OK.yaml
 
 oc create secret generic asperanoded-creds --from-literal=NODE_USER=nodeuser --from-literal=NODE_PASS=`uuidgen` -n $PROJECT
 
-cat /root/cp4i/asperaHstsInstance/asperaHstsInstance.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" | sed "s/{###PROVIDE_YOUR_STORAGECLASSFS_HERE###}/$STORAGECLASSFS/g"  | sed "s/{###PROVIDE_YOUR_ASPERA_LICENCE_CP4X_HERE###}/$ASPERALICENCE/g"  | sed "s/{###PROVIDE_YOUR_ASPERA_DIRECTORY_HERE###}/$ASPERADIRECTORY/g"  >/tmp/$PROJECT/asperaHstsInstance/asperaHstsInstance_OK.yaml
-
+cat /root/cp4i/asperaHstsInstance/asperaHstsInstance.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" | sed "s/{###PROVIDE_YOUR_STORAGECLASSFS_HERE###}/$STORAGECLASSFS/g"  | sed "s/{###PROVIDE_YOUR_ASPERA_LICENCE_HERE###}/$ASPERALICENCE/g" >/tmp/$PROJECT/asperaHstsInstance/asperaHstsInstance_OK.yaml
 oc apply -f >/tmp/$PROJECT/asperaHstsInstance/asperaHstsInstance_OK.yaml
