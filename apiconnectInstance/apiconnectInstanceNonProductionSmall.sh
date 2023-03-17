dc1f81be99ae9965d2e204522ab8e76beb90c695
alexandrezanetti <alexandre_zanetti_almeida@yahoo.com.br>
2023-03-17 16:46:50 -0300

    Update apiconnectInstanceNonProductionSmall.sh

--- apiconnectInstance/apiconnectInstanceNonProductionSmall.sh
+++ apiconnectInstance/apiconnectInstanceNonProductionSmall.sh
@@ -10,7 +10,7 @@ chmod a+x /tmp/$PROJECT/apiconnectInstance
 cat /root/cp4i/apiconnectInstance/apiconnectInstanceNonProductionSmall.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" | sed "s/{###PROVIDE_YOUR_STORAGECLASSBLOCK_HERE###}/$STORAGECLASSBLOCK/g"  | sed "s/{###PROVIDE_YOUR_APIC_LICENCE_HERE###}/$APICLICENCE/g" >/tmp/$PROJECT/apiconnectInstance/apiconnectInstanceNonProductionSmall_OK.yaml
 oc apply -f /tmp/$PROJECT/apiconnectInstance/apiconnectInstanceNonProductionSmall_OK.yaml
 
-oc get routes --all-namespaces | grep -v "gateway-manager" | grep -i "-apic-gw-gateway-" > /tmp/url-console-cp4i.txt
+oc get routes --all-namespaces | grep -v "gateway-manager" | grep -i "apic-gw-gateway-" > /tmp/url-console-cp4i.txt
 export APICGWURLCON=https://$(awk '/-apic-gw-gateway-/ {print $3}' /tmp/url-console-cp4i.txt)
 echo "##############################################"
 echo "##############################################"
