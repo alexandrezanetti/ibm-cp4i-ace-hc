b7d010bfcfdec11ae8dc39da3b4abe36dfd88f8a
alexandrezanetti <alexandre_zanetti_almeida@yahoo.com.br>
2023-03-17 11:33:02 -0300

    Update apiconnectInstanceNonProdctionSmall.sh

--- apiconnectInstance/apiconnectInstanceNonProdctionSmall.sh
+++ apiconnectInstance/apiconnectInstanceNonProdctionSmall.sh
@@ -7,6 +7,6 @@ echo $APICLICENCE
 mkdir -p /tmp/$PROJECT/apiconnectInstance
 chmod a+x /tmp/$PROJECT/apiconnectInstance
 
-cat /root/cp4i/apiconnectInstance/apiconnectInstance.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" | sed "s/{###PROVIDE_YOUR_STORAGECLASSBLOCK_HERE###}/$STORAGECLASSBLOCK/g"  | sed "s/{###PROVIDE_YOUR_APIC_LICENCE_HERE###}/$APICLICENCE/g" >/tmp/$PROJECT/apiconnectInstance/apiconnectInstance_OK.yaml
-oc apply -f /tmp/$PROJECT/apiconnectInstance/apiconnectInstance_OK.yaml
+cat /root/cp4i/apiconnectInstance/apiconnectInstanceNonProdctionSmall.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" | sed "s/{###PROVIDE_YOUR_STORAGECLASSBLOCK_HERE###}/$STORAGECLASSBLOCK/g"  | sed "s/{###PROVIDE_YOUR_APIC_LICENCE_HERE###}/$APICLICENCE/g" >/tmp/$PROJECT/apiconnectInstance/apiconnectInstance_OK.yaml
+oc apply -f /tmp/$PROJECT/apiconnectInstance/apiconnectInstanceNonProdctionSmall_OK.yaml
 
