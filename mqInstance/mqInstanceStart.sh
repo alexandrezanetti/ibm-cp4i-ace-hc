a6cf824125be83f482b975886176218b7688dc45
alexandrezanetti <alexandre_zanetti_almeida@yahoo.com.br>
2023-03-20 13:30:11 -0300

    Create mqInstanceStart.sh

---
+++ mqInstance/mqInstanceStart.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo $PROJECT
+echo $STORAGECLASSFS
+echo $MQLICENCE
+
+mkdir -p /tmp/$PROJECT/apiconnectInstance
+chmod a+x /tmp/$PROJECT/apiconnectInstance
+
+cat /root/cp4i/apiconnectInstance/apiconnectInstanceNonProductionSmall.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" | sed "s/{###PROVIDE_YOUR_STORAGECLASSBLOCK_HERE###}/$STORAGECLASSBLOCK/g"  | sed "s/{###PROVIDE_YOUR_APIC_LICENCE_HERE###}/$APICLICENCE/g" >/tmp/$PROJECT/apiconnectInstance/apiconnectInstanceNonProductionSmall_OK.yaml
+oc apply -f /tmp/$PROJECT/apiconnectInstance/apiconnectInstanceNonProductionSmall_OK.yaml
+
+oc get routes --all-namespaces | grep -v "gateway-manager" | grep -i "apic-gw-gateway-" > /tmp/url-console-cp4i.txt
+export APICGWURLCON=https://$(awk '/-apic-gw-gateway-/ {print $3}' /tmp/url-console-cp4i.txt)
+echo "##############################################"
+echo "##############################################"
+echo "Url do CP4I APIC Gateway: $APICGWURLCON"
+echo "##############################################"
+echo "##########################################
