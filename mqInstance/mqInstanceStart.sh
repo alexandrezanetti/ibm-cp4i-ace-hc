108444f5e445e3f620e236f728cfac07781acd10
alexandrezanetti <alexandre_zanetti_almeida@yahoo.com.br>
2023-03-20 13:37:10 -0300

    Update mqInstanceStart.sh

--- mqInstance/mqInstanceStart.sh
+++ mqInstance/mqInstanceStart.sh
@@ -4,11 +4,17 @@ echo $PROJECT
 echo $STORAGECLASSFS
 echo $MQLICENCE
 
-mkdir -p /tmp/$PROJECT/apiconnectInstance
-chmod a+x /tmp/$PROJECT/apiconnectInstance
+mkdir -p /tmp/$PROJECT/mqInstance
+chmod a+x /tmp/$PROJECT/mqInstance
 
-cat /root/cp4i/apiconnectInstance/apiconnectInstanceNonProductionSmall.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" | sed "s/{###PROVIDE_YOUR_STORAGECLASSBLOCK_HERE###}/$STORAGECLASSBLOCK/g"  | sed "s/{###PROVIDE_YOUR_APIC_LICENCE_HERE###}/$APICLICENCE/g" >/tmp/$PROJECT/apiconnectInstance/apiconnectInstanceNonProductionSmall_OK.yaml
-oc apply -f /tmp/$PROJECT/apiconnectInstance/apiconnectInstanceNonProductionSmall_OK.yaml
+cat /root/cp4i/mqInstance/mqInstanceSecret.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" | sed "s/{###PROVIDE_YOUR_STORAGECLASSFS_HERE###}/$STORAGECLASSFS/g"  | sed "s/{###PROVIDE_YOUR_MQ_LICENCE_HERE###}/$MQLICENCE/g" > /tmp/$PROJECT/mqInstance/mqInstanceSecret_OK.yaml
+oc apply -f /tmp/$PROJECT/mqInstance/mqInstanceSecret_OK.yaml
+
+cat /root/cp4i/mqInstance/mqInstanceConfigMapMQSC.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" | sed "s/{###PROVIDE_YOUR_STORAGECLASSFS_HERE###}/$STORAGECLASSFS/g"  | sed "s/{###PROVIDE_YOUR_MQ_LICENCE_HERE###}/$MQLICENCE/g" > /tmp/$PROJECT/mqInstance/mqInstanceConfigMapMQSC_OK.yaml
+oc apply -f /tmp/$PROJECT/mqInstance/mqInstanceConfigMapMQSC_OK.yaml
+
+cat /root/cp4i/mqInstance/mqInstanceQMgrStart.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" | sed "s/{###PROVIDE_YOUR_STORAGECLASSFS_HERE###}/$STORAGECLASSFS/g"  | sed "s/{###PROVIDE_YOUR_MQ_LICENCE_HERE###}/$MQLICENCE/g" > /tmp/$PROJECT/mqInstance/mqInstance/mqInstanceQMgrStart_OK.yaml
+oc apply -f /tmp/$PROJECT/mqInstance/mqInstance/mqInstanceQMgrStart_OK.yaml
 
 oc get routes --all-namespaces | grep -v "gateway-manager" | grep -i "apic-gw-gateway-" > /tmp/url-console-cp4i.txt
 export APICGWURLCON=https://$(awk '/-apic-gw-gateway-/ {print $3}' /tmp/url-console-cp4i.txt)
