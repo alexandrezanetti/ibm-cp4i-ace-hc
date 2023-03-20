c04c5ae2d2797a5297631d411b7ac2f28f0a87fe
alexandrezanetti <alexandre_zanetti_almeida@yahoo.com.br>
2023-03-20 16:19:20 -0300

    Update mqInstanceStart.sh

--- mqInstance/mqInstanceStart.sh
+++ mqInstance/mqInstanceStart.sh
@@ -16,10 +16,11 @@ oc apply -f /tmp/$PROJECT/mqInstance/mqInstanceConfigMapMQSC_OK.yaml
 cat /root/cp4i/mqInstance/mqInstanceQMgrStart.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" | sed "s/{###PROVIDE_YOUR_STORAGECLASSFS_HERE###}/$STORAGECLASSFS/g"  | sed "s/{###PROVIDE_YOUR_MQ_LICENCE_HERE###}/$MQLICENCE/g" > /tmp/$PROJECT/mqInstance/mqInstanceQMgrStart_OK.yaml
 oc apply -f /tmp/$PROJECT/mqInstance/mqInstanceQMgrStart_OK.yaml
 
-oc get routes --all-namespaces | grep -v "gateway-manager" | grep -i "apic-gw-gateway-" > /tmp/url-console-cp4i.txt
-export APICGWURLCON=https://$(awk '/-apic-gw-gateway-/ {print $3}' /tmp/url-console-cp4i.txt)
+ oc describe pod cp4i-mq-qmgr-start-ibm-mq-0 -n cp4i | grep MQ_ZEN_BASE_URI
+oc get routes --all-namespaces | grep -v "cp4i-mq-qmgr" | grep -i "-mq-qmgr-" > /tmp/url-console-mq.txt
+export MQURLCON=https://$(awk '/-mq-qmgr-"/ {print $3}' /tmp/url-console-mq.txt)
 echo "##############################################"
 echo "##############################################"
-echo "Url do CP4I APIC Gateway: $APICGWURLCON"
+echo "Url do CP4I MQ: $MQURLCON"
 echo "##############################################"
 echo "##########################################"
