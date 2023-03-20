fd4a5986dd438be52d093d771c1a5a5784823183
alexandrezanetti <alexandre_zanetti_almeida@yahoo.com.br>
2023-03-20 08:48:11 -0300

    Update platformNavigatorInstance.sh

--- platformNavigatorInstance/platformNavigatorInstance.sh
+++ platformNavigatorInstance/platformNavigatorInstance.sh
@@ -12,6 +12,11 @@ oc apply -f /tmp/$PROJECT/platformNavigatorInstance/platformNavigatorInstance_OK
 
 oc get secret platform-auth-idp-credentials -n ibm-common-services -o go-template --template="{{.data.admin_password|base64decode}}" > /tmp/$PROJECT/platformNavigatorInstance/bm-common-services-platform-auth-idp-credentials_OK.yaml
 
+echo "Waiting 30 seconds"
+for i in {1..30}; do  echo "Loop time "$i;   sleep 1s; done
+
+echo "Waiting restart of all Nodes (Masters and Workers)"
+while (oc get routes --all-namespaces | grep -i platform-navigator-ui); do sleep 1 ; done
 
 oc get routes --all-namespaces | grep -i platform-navigator-ui > /tmp/url-console-cp4i.txt
 export CP4IURLCON=https://$(awk '/ cp4i-platform-navigator-ui-pn-/ {print $3}' /tmp/url-console-cp4i.txt)
