fb44084ef9002465cb7011286fa7d75f2707b0c0
alexandrezanetti <alexandre_zanetti_almeida@yahoo.com.br>
2023-03-20 16:32:05 -0300

    Update mqInstanceStart.sh

--- mqInstance/mqInstanceStart.sh
+++ mqInstance/mqInstanceStart.sh
@@ -16,8 +16,7 @@ oc apply -f /tmp/$PROJECT/mqInstance/mqInstanceConfigMapMQSC_OK.yaml
 cat /root/cp4i/mqInstance/mqInstanceQMgrStart.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" | sed "s/{###PROVIDE_YOUR_STORAGECLASSFS_HERE###}/$STORAGECLASSFS/g"  | sed "s/{###PROVIDE_YOUR_MQ_LICENCE_HERE###}/$MQLICENCE/g" > /tmp/$PROJECT/mqInstance/mqInstanceQMgrStart_OK.yaml
 oc apply -f /tmp/$PROJECT/mqInstance/mqInstanceQMgrStart_OK.yaml
 
- oc describe pod cp4i-mq-qmgr-start-ibm-mq-0 -n cp4i | grep MQ_ZEN_BASE_URI
-oc get routes --all-namespaces | grep -v "cp4i-mq-qmgr" | grep -i "-mq-qmgr-" > /tmp/url-console-mq.txt
+oc get routes --all-namespaces | grep "cp4i-mq-qmgr" > /tmp/url-console-mq.txt
 export MQURLCON=https://$(awk '/-mq-qmgr-"/ {print $3}' /tmp/url-console-mq.txt)
 echo "##############################################"
 echo "##############################################"
