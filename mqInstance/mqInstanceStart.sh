28c0c04a4ec592212f431844d46ac4c9c43e3af3
alexandrezanetti <alexandre_zanetti_almeida@yahoo.com.br>
2023-03-20 17:21:07 -0300

    Update mqInstanceStart.sh

--- mqInstance/mqInstanceStart.sh
+++ mqInstance/mqInstanceStart.sh
@@ -16,8 +16,8 @@ oc apply -f /tmp/$PROJECT/mqInstance/mqInstanceConfigMapMQSC_OK.yaml
 cat /root/cp4i/mqInstance/mqInstanceQMgrStart.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" | sed "s/{###PROVIDE_YOUR_STORAGECLASSFS_HERE###}/$STORAGECLASSFS/g"  | sed "s/{###PROVIDE_YOUR_MQ_LICENCE_HERE###}/$MQLICENCE/g" > /tmp/$PROJECT/mqInstance/mqInstanceQMgrStart_OK.yaml
 oc apply -f /tmp/$PROJECT/mqInstance/mqInstanceQMgrStart_OK.yaml
 
-while (oc get routes --all-namespaces | grep -v "${PROJECT}-mq-qmgr-start-ibm-mq-qm-"); do sleep 1 ; done
-oc get routes --all-namespaces | grep "${PROJECT}-mq-qmgr-start-ibm-mq-qm-" > /tmp/url-console-mq.txt
+while (oc get pod | grep -i "${PROJECT}-mq-qmgr-start-ibm-mq-0" | grep -i "Running" ) ; do sleep 1 ; done
+oc get pod --all-namespaces | grep ""${PROJECT}-mq-qmgr-start-ibm-mq-0" > /tmp/url-console-mq.txt
 export MQURLCON=https://$(awk '/-mq-qmgr-"/ {print $3}' /tmp/url-console-mq.txt)
 export MQUSER=$(oc get secret "${PROJECT}-mq-secret" -n "${PROJECT}" -o go-template --template="{{.data.user|base64decode}}")
 export MQPASSWORD=$(oc get secret "${PROJECT}-mq-secret" -n "${PROJECT}" -o go-template --template="{{.data.password|base64decode}}")
