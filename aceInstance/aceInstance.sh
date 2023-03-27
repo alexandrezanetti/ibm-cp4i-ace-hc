b8277a731ec0f3eba756fc7ab35631d9487e52ab
alexandrezanetti <alexandre_zanetti_almeida@yahoo.com.br>
2023-03-27 14:56:30 -0300

    Update aceInstance.sh

--- aceInstance/aceInstance.sh
+++ aceInstance/aceInstance.sh
@@ -7,9 +7,15 @@ echo "ACEUSERNAME: ${ACEUSERNAME}"
 echo "ACEPASSWORD: ${ACEPASSWORD}"
 echo "ACEHASH: ${ACEHASH}"
 
-mkdir -p /tmp/$PROJECT/aceinstance
-chmod a+x /tmp/$PROJECT/aceinstance
+mkdir -p /tmp/$PROJECT/aceInstance
+chmod a+x /tmp/$PROJECT/aceInstance
 
-cat /root/cp4i/aceinstance/aceinstance.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" | sed "s/{###PROVIDE_YOUR_STORAGECLASSBLOCK_HERE###}/$STORAGECLASSBLOCK/g"  | sed "s/{###PROVIDE_YOUR_HASH_HERE###}/${ACEHASH}/g" > /tmp/$PROJECT/aceinstance/aceinstance_OK.yaml
-oc apply -f /tmp/$PROJECT/aceinstance/aceinstance_OK.yaml
+cat /root/cp4i/aceInstance/aceInstance.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" | sed "s/{###PROVIDE_YOUR_STORAGECLASSFS_HERE###}/$STORAGECLASSFS/g"  | sed "s/{###PROVIDE_YOUR_STORAGECLASSFS_HERE###}/$STORAGECLASSFS/g"  | sed "s/{###PROVIDE_YOUR_ACE_LICENCE_CP4X_HERE###}/$ACELICENCE/g"  | sed "s/{###PROVIDE_YOUR_HASH_HERE###}/${ACEHASH}/g" > /tmp/$PROJECT/aceInstance/aceInstance_OK.yaml
+oc apply -f /tmp/$PROJECT/aceInstance/aceInstance_OK.yaml
+
+cat /root/cp4i/aceInstance/aceInstance.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" | sed "s/{###PROVIDE_YOUR_STORAGECLASSFS_HERE###}/$STORAGECLASSFS/g"  | sed "s/{###PROVIDE_YOUR_STORAGECLASSFS_HERE###}/$STORAGECLASSFS/g"  | sed "s/{###PROVIDE_YOUR_ACE_LICENCE_CP4X_HERE###}/$ACELICENCE/g"  | sed "s/{###PROVIDE_YOUR_HASH_HERE###}/${ACEHASH}/g" > /tmp/$PROJECT/aceInstance/aceInstance_OK.yaml
+oc apply -f /tmp/$PROJECT/aceInstance/aceInstance_OK.yaml
+
+cat /root/cp4i/aceInstance/aceInstance.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" | sed "s/{###PROVIDE_YOUR_STORAGECLASSFS_HERE###}/$STORAGECLASSFS/g"  | sed "s/{###PROVIDE_YOUR_STORAGECLASSFS_HERE###}/$STORAGECLASSFS/g"  | sed "s/{###PROVIDE_YOUR_ACE_LICENCE_CP4X_HERE###}/$ACELICENCE/g"  | sed "s/{###PROVIDE_YOUR_HASH_HERE###}/${ACEHASH}/g" > /tmp/$PROJECT/aceInstance/aceInstance_OK.yaml
+oc apply -f /tmp/$PROJECT/aceInstance/aceInstance_OK.yaml
 
