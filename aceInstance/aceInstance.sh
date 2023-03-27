94c45ef33639a430758bd14b3ed3c94e1c88ea23
alexandrezanetti <alexandre_zanetti_almeida@yahoo.com.br>
2023-03-27 15:21:47 -0300

    Update aceInstance.sh

--- aceInstance/aceInstance.sh
+++ aceInstance/aceInstance.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export ACEUSERNAME=admin
 export ACEPASSWORD=admin
-export ACEHASH=$(echo '{"authType":"BASIC_AUTH","credentials":{"username":"${ACEUSERNAME}","password":"${ACEPASSWORD}"}}' | base64)
+export ACEHASH=$(echo "{\"authType\":\"BASIC_AUTH\",\"credentials\":{\"username\":\"${ACEUSERNAME}\",\"password\":\"${ACEPASSWORD}\"}}" | base64)
 
 echo "ACEUSERNAME: ${ACEUSERNAME}"
 echo "ACEPASSWORD: ${ACEPASSWORD}"
