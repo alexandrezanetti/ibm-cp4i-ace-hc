08a412be2d3c0fdb6771aa0384c0f099850df5b5
alexandrezanetti <alexandre_zanetti_almeida@yahoo.com.br>
2023-04-11 08:45:09 -0300

    Update platformNavigatorInstance.sh

--- platformNavigatorInstance/platformNavigatorInstance.sh
+++ platformNavigatorInstance/platformNavigatorInstance.sh
@@ -16,15 +16,20 @@ echo "Waiting 30 seconds"
 for i in {1..30}; do  echo "Loop time "$i;   sleep 1s; done
 
 echo "Waiting restart of all Nodes (Masters and Workers)"
+export i=1
 while true
 do
-        echo "Aguarde enquanto os recursos são criados... em geral 30/40min!"
+        clear
+        echo "Aguarde enquanto os recursos são criados... em geral 30/40min! - Loop $i"
 	oc get pod | grep "${PROJECT}-platform-navigator-ui" | grep Running
-	if [[ $? -eq 0 ]]
-	then
-		break
-	fi
+        if [[ $? -eq 0 ]]
+        then
+                break
+        fi
+        sleep 60s
+        $i=$i+1
 done
+
 oc get routes --all-namespaces | grep -i platform-navigator-ui > /tmp/url-console-cp4i.txt
 export CP4IURLCON=https://$(awk '/ cp4i-platform-navigator-ui-pn-/ {print $3}' /tmp/url-console-cp4i.txt)
 export CP4IPNPASSWORD=$(oc get secret platform-auth-idp-credentials -n ibm-common-services -o go-template --template="{{.data.admin_password|base64decode}}")
