3e94d1b2a71de7e3613f9965f75fcc71c3a2d923
alexandrezanetti <alexandre_zanetti_almeida@yahoo.com.br>
2023-04-11 08:45:56 -0300

    Update platformNavigatorInstance.sh

--- platformNavigatorInstance/platformNavigatorInstance.sh
+++ platformNavigatorInstance/platformNavigatorInstance.sh
@@ -21,6 +21,7 @@ while true
 do
         clear
         echo "Aguarde enquanto os recursos são criados... em geral 30/40min! - Loop $i"
+	echo "Pod: ${PROJECT}-platform-navigator-ui"
 	oc get pod | grep "${PROJECT}-platform-navigator-ui" | grep Running
         if [[ $? -eq 0 ]]
         then
