8e64483a95bf28c2b5d2ef757e1f253c32bfd4ee
alexandrezanetti <alexandre_zanetti_almeida@yahoo.com.br>
2023-04-18 08:57:15 -0300

    Update asperaHstsInstance.sh

--- asperaHstsInstance/asperaHstsInstance.sh
+++ asperaHstsInstance/asperaHstsInstance.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+echo "Check your IBM Aspera License - https://www.ibm.com/cloud/aspera/license-key" 
+echo "ASPERALICENSE: ${ASPERALICENSE}"
+if [ ${ASPERALICENSE} = "{###PROVIDE_YOUR_ASPERA_LICENCE_CP4X_HERE###}" ]; then echo "Please provide your IBM Aspera License - Check https://www.ibm.com/cloud/aspera/license-key"; exit 999; fi
+
 echo $PROJECT
 echo $STORAGECLASSFS
-echo $ASPERALICENCE
+echo $ASPERALICENSE
 
 mkdir -p /tmp/$PROJECT/asperaHstsInstance
 chmod a+x /tmp/$PROJECT/asperaHstsInstance
@@ -12,5 +16,5 @@ oc apply -f /tmp/$PROJECT/asperaHstsInstance/asperaHstsInstancePvc_OK.yaml
 
 oc create secret generic asperanoded-creds --from-literal=NODE_USER=nodeuser --from-literal=NODE_PASS='uuidgen' -n $PROJECT
 
-cat ~/cp4i/asperaHstsInstance/asperaHstsInstance.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" | sed "s/{###PROVIDE_YOUR_STORAGECLASSFS_HERE###}/$STORAGECLASSFS/g"  | sed "s/{###PROVIDE_YOUR_ASPERA_LICENCE_HERE###}/$ASPERALICENCE/g" >/tmp/$PROJECT/asperaHstsInstance/asperaHstsInstance_OK.yaml
+cat ~/cp4i/asperaHstsInstance/asperaHstsInstance.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" | sed "s/{###PROVIDE_YOUR_STORAGECLASSFS_HERE###}/$STORAGECLASSFS/g"  | sed "s/{###PROVIDE_YOUR_ASPERA_LICENSE_HERE###}/$ASPERALICENSE/g" >/tmp/$PROJECT/asperaHstsInstance/asperaHstsInstance_OK.yaml
 oc apply -f /tmp/$PROJECT/asperaHstsInstance/asperaHstsInstance_OK.yaml
